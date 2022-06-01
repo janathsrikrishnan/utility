46056867d12e8d4f9dd3ebc799c9772b168fc498
root <root@localhost.localdomain>
2022-06-01 11:28:43 -0400

    status added

--- utility.sh
+++ utility.sh
@@ -1,4 +1,3 @@
-
 red=$(tput setaf 1)
 cyan=$(tput setaf 6)
 reset=$(tput sgr0)
@@ -18,14 +17,37 @@ while [ $exit -eq 0 ]
 do
     echo -n "command:"
     read command
+    
     if [[ $command == "quit" ]]; then
         exit=1
+    
     elif [[ $command == 'status'  ]]; then
         echo -n "Enter pid or name (default pid):"
         read name
-        ps aux > process.txt
+    
+    
         if [[ $name == 'name' ]]; then
-            awk '{print $0}' process.txt
+            echo "[+]current running process are:"
+            ps aux
+            ps aux > process.txt
+            echo -n "[+]Enter the process name(command):"
+            read process_name
+
+            echo "[+]process ids"
+            awk -v Name=$process_name '$11 ~ Name {printf "%s ", $2}END{printf "\n"}' process.txt
         fi
+
+        echo -n "[+]Enter the pid: "
+        read pid
+
+        cd /proc/$pid
+        ls -a
+
+        echo -n "[+]show(pid:$pid):"
+        read dir
+
+        cat $dir
+
+
     fi
 done
